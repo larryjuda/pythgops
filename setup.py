32849df97443fe2fdbea024f8e48cfb2ad9478cb
Larry <55913828+larryjuda@users.noreply.github.com>
2023-02-18 15:37:30 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author="Larry Juda",
     author_email='larry.juda@thehutgroup.com',
     packages=['pythgops'],
-    url='https://github.com/gmyrianthous/example-publish-pypi',
+    url='https://github.com/larryjuda/pythgops',
     install_requires=[
           'pandas',
           'datetime',
